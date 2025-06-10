89a29188da5bdf1941c44724f3a120928ad6c1c0
Mati Melkamu <matimelkamu15@gmail.com>
2025-06-10 12:23:35 +0300

    fix x

--- frontend/src/utils/RedirectAuthenticated.jsx
+++ frontend/src/utils/RedirectAuthenticated.jsx
@@ -1,10 +1,41 @@
-import { Navigate } from "react-router-dom";
+import { Navigate, navigate } from "react-router-dom";
 import useAuthStore from "../store/auth.store";
 
 function RedirectAuthenticated({ children }) {
   const { user, isAuthenticated } = useAuthStore();
+
+  let redirect;
+
+  switch (user.role) {
+    case "System Admin":
+      redirect = "/admin-dashboard";
+      break;
+
+    case "Chief CEO":
+      redirect = "/admin";
+      break;
+
+    case "CEO":
+      redirect = "/admin";
+      break;
+    case "Worker":
+      redirect = "/admin";
+      break;
+
+    case "Minister":
+      redirect = "/admin";
+      break;
+
+    case "Strategic Unit":
+      redirect = "/admin";
+      break;
+
+    default:
+      toast.error("Unknown role. Access denied.");
+  }
+
   if (isAuthenticated) {
-    <Navigate to={``} />;
+    <Navigate to={`${user.role}`} />;
   }
   return children;
 }
